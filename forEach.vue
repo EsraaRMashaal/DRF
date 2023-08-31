8f661dfd8c37955cf2ca42731cc902d158c53eba
Esraa R. Mashaal <31254134+EsraaRMashaal@users.noreply.github.com>
2023-08-31 09:54:05 +0300

    Create forEach.vue

---
+++ forEach.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+ // Best way to wait for .forEach() to complete
+  async loopOnData() {
+        this.data.forEach((item, index, array) => { // loop on all data then call api depend on each record
+          console.log('Data :', item);
+          API.finally(() => {
+            if (index === this.data.length -1){ // last iteration 
+               console.log('Done --------');
+            }
+          });
+        });
+    }
+  
+</script>
