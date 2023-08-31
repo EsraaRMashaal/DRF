61bb8234422344549d0427d6c72f04dd0f32f13f
Esraa R. Mashaal <31254134+EsraaRMashaal@users.noreply.github.com>
2023-08-31 11:15:33 +0300

    Update forEach.vue

--- forEach.vue
+++ forEach.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 
- // Best way to wait for .forEach() to complete
+ // Best way to wait for .forEach() to complete all APIs calls
   async loopOnData() {
         this.data.forEach(item => { // loop on all data then call api depend on each record
           console.log('Data :', item);
